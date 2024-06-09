3234df820cad4e420c22103102b72b7c792c9f4e
loydie16 <johnloydtalagtag16@gmail.com>
2024-06-10 02:37:59 +0800

    setup appwrite

--- app/(auth)/sign-in.jsx
+++ app/(auth)/sign-in.jsx
@@ -1,23 +1,39 @@
-import { View, Text, ScrollView, Image } from 'react-native'
-import React, { useState } from 'react'
-import { SafeAreaView } from 'react-native-safe-area-context'
-import { images } from '../../constants'
-import FormField from '../../components/FormField'
-import CustomButton from '../../components/CustomButton'
-import { Link } from 'expo-router'
+import { View, Text, ScrollView, Image, Alert } from "react-native";
+import React, { useState } from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { images } from "../../constants";
+import FormField from "../../components/FormField";
+import CustomButton from "../../components/CustomButton";
+import { Link, router } from "expo-router";
+import { signIn } from "../../lib/appwrite";
 
 const SignIn = () => {
-
   const [form, setForm] = useState({
-    email: '',
-    password: '',
-  })
+    email: "",
+    password: "",
+  });
 
-  const [isSubmitting, setIsSubmitting] = useState(false)
+  const [isSubmitting, setIsSubmitting] = useState(false);
 
-  const submit = () => {
+  const submit = async () => {
+    if (!form.email || !form.password) {
+      Alert.alert("Error", "Please fill in all the fields");
+    }
 
-  }
+    setIsSubmitting(true);
+
+    try {
+      await signIn(form.email, form.password);
+
+      //set it to global state...
+
+      router.replace("/home");
+    } catch (error) {
+      Alert.alert("Error", error.message);
+    } finally {
+      setIsSubmitting(false);
+    }
+  };
 
   return (
     <SafeAreaView className="bg-primary h-full">
@@ -55,17 +71,20 @@ const SignIn = () => {
           />
 
           <View className="justify-center pt-5 flex-row gap-2">
-             <Text className="text-lg text-gray-100 font-pregular">
+            <Text className="text-lg text-gray-100 font-pregular">
               Don't have account?
-             </Text>
-             <Link href="/sign-up" className='text-lg font-psemibold text-secondary'>
+            </Text>
+            <Link
+              href="/sign-up"
+              className="text-lg font-psemibold text-secondary"
+            >
               Sign Up
-             </Link>
+            </Link>
           </View>
         </View>
       </ScrollView>
     </SafeAreaView>
   );
-}
+};
 
-export default SignIn
+export default SignIn;
